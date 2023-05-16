27d04c9df8dca2108c6a874c4ccff5be51be8e4f
SHVEDenchik <vmiche@yandex.ru>
2023-05-16 14:55:52 +0300

    Semaphore

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,13 +2,12 @@
 {
     static void Main(string[] args)
     {
-        string myName = "Max";
-        byte age = 29;
-        bool youPet = true;
-        double youShoesSize = 42;
-        Console.WriteLine("Меня зовут " + myName);
-        Console.WriteLine("Мой возраст " + age);
-        Console.WriteLine("Имеете ли вы домашнего питомца? " + youPet);
-        Console.WriteLine("Ваш размер обуви " + youShoesSize);
+        enum Seemaphore
+    {
+
+        red = 100,
+        yellow = 200,
+        green = 300
+    }
     }
 }
