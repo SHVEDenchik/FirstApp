f409c6bc496e585f2e6bc5b04a9b7a379fdfa443
SHVEDenchik <vmiche@yandex.ru>
2023-06-01 10:52:50 +0300

    Вызов метода ShowColor

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,43 +4,16 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        (string Name, string LastName, string Login, int LoginLenght, bool HasPet, double PetAge, string[] favColors, double Age) User;     //создаем кортеж
+        var favcolors = new String[3];
 
-        for (int countUsers = 0; countUsers < 3; countUsers++)                                                                              // делаем три итерации для трех пользователей
+        for (int i = 0; i < favcolors.Length; i++)
         {
+            favcolors[i] = ShowColor();                                 // Записываем данные в масив вызовом метода ShowColor, который считывает данные с клавиатуры
+        }
 
-            Console.WriteLine("Введите ваше имя");
-            User.Name = Console.ReadLine();                                                                                                 //записываем данные в поле кортежа
-
-            Console.WriteLine("Введите вашу фамилию");
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("Укажите ваш логин");
-            User.Login = Console.ReadLine();
-
-            User.LoginLenght = User.Login.Length;
-
-            Console.WriteLine("Укажите, есть ли у Вас домашнее животное? Да / нет");
-            var result = Console.ReadLine();
-            if (result == "Да")
-            {
-                User.HasPet = true;
-            }
-            else
-            {
-                User.HasPet = false;
-            }
-
-            Console.WriteLine("Укажите Ваш возраст");
-            User.Age = double.Parse(Console.ReadLine());                                                                                    //Парсим введенную строку в формат double
-
-            User.favColors = new string[3];                                                                                                 // элемент кортежа как созданный масив из 3 значений
-            Console.WriteLine("Укажите 3 своих любимых цвета");
-
-            for (int i = 0; i < User.favColors.Length; i++)
-            {
-                User.favColors[i] = Console.ReadLine();
-            }
+        for (int i = 0;i < favcolors.Length; i++)
+        {
+            Console.WriteLine(favcolors[i]);
         }
     }
 
